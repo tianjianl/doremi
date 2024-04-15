b76587702f2878da112674f57a3d1e3d0e868bb6
Tianjian Li <litianjian_1998@sina.com>
2024-04-15 13:26:12 -0400

    Update rp_constants.sh

--- rp_constants.sh
+++ rp_constants.sh
@@ -9,7 +9,7 @@ RP_DIR=/scratch/tli104/slimpajama-split
 PREPROCESSED_RP_DIR=/scratch/tli104/slimpajama-split/rp_preprocessed  # will be created by scripts/run_filter_domains.sh
 
 MODEL_OUTPUT_DIR=/scratch/tli104/rm_model_output_dir
-WANDB_API_KEY=ffa04cba19ef3ea0e69dcd5c38837c2634f03c82  # Weights and Biases key for logging
+WANDB_API_KEY=  # Weights and Biases key for logging
 PARTITION=defq # for slurm
 mkdir -p ${CACHE}
 mkdir -p ${MODEL_OUTPUT_DIR}
